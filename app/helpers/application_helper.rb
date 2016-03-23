00d884007f427f35bacec9fbb65dd51207b4df0a
Yaroslav Melnychuk <yamelnychuk@gmail.com>
2016-03-24 01:42:31 +0200

    Finish user edit, update, index, and destroy actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,9 +13,4 @@ module ApplicationHelper
     fill_in "Password", with: user.password
     click_button "Sign in"
   end
-
-  RSpec::Matchers.define :have_error_message do |message|
-    match do |page|
-      expect(page).to have_selector('div.alert.alert-error', text: message)
-  end
 end
