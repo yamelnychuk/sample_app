539a7ba0656dabe1ac812fe0124b3e3d76528bfe
Yaroslav Melnychuk <yamelnychuk@gmail.com>
2016-03-23 18:32:51 +0200

    User Controllerbug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
