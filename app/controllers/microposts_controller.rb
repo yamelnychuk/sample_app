7cb752d79cbd9fb1bcb848c80147b553ac500685
Yaroslav Melnychuk <yamelnychuk@gmail.com>
2023-12-09 15:23:20 +0200

    test commit

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -18,6 +18,11 @@ class MicropostsController < ApplicationController
     redirect_to root_url
   end
 
+  def ok_func
+    @micropost.destroy
+    redirect_to root_url
+  end
+
   private
 
     def micropost_params
